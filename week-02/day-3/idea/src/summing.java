5fa617b69c3ab33ec0422051a723cb832624d325
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-22 14:42:40 +0100

    Exercise: summit done

---
+++ week-02/day-3/idea/src/summing.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Adam on 2017. 03. 22..
+ */
+import java.util.Scanner;
+public class summing {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please type a number!");
+    int num = scanner.nextInt();
+    System.out.println(sum(num));
+  }
+  public static int sum (int p1) {
+    int sum = p1*(p1+1)/2;
+    return sum;
+  }
+}
