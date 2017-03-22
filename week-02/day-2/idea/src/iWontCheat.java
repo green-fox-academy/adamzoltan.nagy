b946e7d657e5910c772c4f1247031c804a8d6222
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-22 11:19:39 +0100

    Exercise 23 done

---
+++ week-02/day-2/idea/src/iWontCheat.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Adam on 2017. 03. 22..
+ */
+public class iWontCheat {
+  public static void main(String[] args) {
+
+    for (int i = 1; i < 101; i++) {
+      System.out.println(i + ": I Won't cheat on the exam!");
+    }
+  }
+}
+
