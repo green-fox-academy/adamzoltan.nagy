47e3781a41ed9bbc03890d98327399c1a9a61fdd
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-22 14:16:09 +0100

    Exercise: GreeterFunction done

---
+++ week-02/day-3/idea/src/greeterFunction.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Adam on 2017. 03. 22..
+ */
+public class greeterFunction {
+  public static void main(String[] args) {
+    String al = "Greenfox";
+    System.out.println(greet(al));
+  }
+  public static String greet (String p1) {
+    String greet = "Greetings dear, " + p1;
+    return greet;
+  }
+
+}
