b40e035f121f20ab12fd5b23204e70adb6e4757d
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-22 15:45:54 +0100

    Exercise: Compare Lenght done

---
+++ week-02/day-3/idea/src/compareLenght.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Adam on 2017. 03. 22..
+ */
+public class compareLenght {
+  public static void main(String[] args) {
+    int [] p1 = {1, 2, 3};
+    int [] p2 = {4, 5};
+    if (p1.length > p2.length) {
+      System.out.println("p1 has more elements than p2");
+    }else {
+      System.out.println("p2 has more elements than p1");
+    }
+  }
+}
