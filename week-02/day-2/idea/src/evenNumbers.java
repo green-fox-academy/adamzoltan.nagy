08e0b096e4d604c424ccecf7af5ea92e9f81af88
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-22 11:23:16 +0100

    Exercise 24 done

---
+++ week-02/day-2/idea/src/evenNumbers.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Adam on 2017. 03. 22..
+ */
+public class evenNumbers {
+  public static void main(String[] args) {
+    for (int i = 0; i < 500; i++) {
+      if (i % 2 == 0 && i != 0){
+        System.out.println(i);
+      }
+    }
+  }
+}
