70568ecac3b776ad8bc262bc398192047bc5eaf9
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-22 14:22:11 +0100

    Exercise: appendA done

---
+++ week-02/day-3/idea/src/appendA.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Adam on 2017. 03. 22..
+ */
+public class appendA {
+  public static void main(String[] args) {
+    String am = "kuty";
+    System.out.println(appendA(am));
+  }
+  public static String appendA(String p1) {
+    String append = p1 + "a";
+    return append;
+  }
+
+}
