93f74575b89c90fad3c142e50a23822c1148512d
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-22 13:57:57 +0100

    Exercise: Doubling done

---
+++ week-02/day-3/idea/src/doubling.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Adam on 2017. 03. 22..
+ */
+public class doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+    int ak2 = doubling(ak);
+    System.out.println(ak2);
+  }
+
+  public static int doubling( int n1) {
+    int doubl = n1 * 2;
+    return doubl;
+  }
+}
