366f6c9978498053762f19aaeae0e00911a88e1d
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-23 11:50:21 +0100

    Exercise 01: Simple Replacement done

---
+++ week-02/day-4/idea/src/exr01SimpleRep.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Adam on 2017. 03. 23..
+ */
+public class exr01SimpleRep {
+  public static void main(String[] args) {
+    String example = "In a dishwasher far far away";
+
+    System.out.println(example.replaceAll("dishwasher", "galaxy"));
+
+  }
+}
