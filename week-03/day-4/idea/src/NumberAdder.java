9d91b808cf6f2f75409f5f6c6156caa6bfd43dbe
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-30 10:18:47 +0200

    Exercise Number Adder done

---
+++ week-03/day-4/idea/src/NumberAdder.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Adam on 2017. 03. 30..
+ */
+public class NumberAdder {
+  public static void main(String[] args) {
+    System.out.println(numberAdder(15));
+  }
+
+  public static int numberAdder (int n) {
+    if (n == 1) {
+      return 1;
+    } else {
+      return n + numberAdder(n-1);
+    }
+  }
+}
