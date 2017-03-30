78cf631b3c1fae5331fffdb73038c6db335e1ee0
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-03-30 10:29:48 +0200

    Exercise Sum Digit done

---
+++ week-03/day-4/idea/src/SumDigit.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Adam on 2017. 03. 30..
+ */
+public class SumDigit {
+  public static void main(String[] args) {
+
+    System.out.println(sumDigit(12345));
+
+  }
+  public static int sumDigit (int n) {
+    if (n == 0) {
+      return 0;
+    } else {
+      return n % 10 + sumDigit(n/10);
+    }
+  }
+}
