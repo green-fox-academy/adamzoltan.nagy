bca76ab073f83f66c2f7dc039f551e38060b2668
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-04 13:24:09 +0200

    Exercise Green Fox Inheritance: Mentro class created

---
+++ week-04/day-2/src/Mentor.java
@@ -0,0 +1,24 @@
+
+public class Mentor extends Person {
+  private String level;
+
+  @Override
+  public void getGoal(){
+    System.out.println( "Educate brilliant junior software developers.");
+  }
+
+  @Override
+  public void introduce(){
+    System.out.println("Hi, I'm " + getName() + " a " + getAge() + " year old " + getGender() + level + " mentor.");
+  }
+
+  Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+
+  Mentor(){
+    super();
+    level = "intermediate";
+  }
+}
