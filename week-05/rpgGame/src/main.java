4e613b489176c4f8a0952e67eb158c0b9ce9dc21
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-11 12:26:23 +0200

    Separate main and board class

--- week-05/rpgGame/src/main.java
+++ week-05/rpgGame/src/main.java
@@ -1,102 +1,6 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
-class Board extends JComponent implements KeyListener {
-
-  int testBoxX;
-  int testBoxY;
-
-  public Board() {
-    testBoxX = 0;
-    testBoxY = 0;
-
-    // set the size of your draw board
-    setPreferredSize(new Dimension(720, 720));
-    setVisible(true);
-  }
-  String hero = "hero-down.png";
-  @Override
-  public void paint(Graphics graphics) {
-    int [][] roadWalls = {
-            {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
-            {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
-            {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-            {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
-            {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
-            {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
-            {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
-            {0, 0, 0, 1, 0, 1, 1, 0, 1, 0},
-    };
-    super.paint(graphics);
-    // here you have a 720x720 canvas
-    // you can create and draw an image using the class below e.g.
-    for (int i = 0; i < 10; i ++) {
-      for (int j = 0; j < 10; j++) {
-        if (roadWalls[i][j] == 0) {
-          PositionedImage image = new PositionedImage("floor.png", j * 72, i * 72);
-          image.draw(graphics);
-        } else { PositionedImage image = new PositionedImage("wall.png", j * 72, i * 72);
-          image.draw(graphics);
-        }
-      }
-    }
-    //graphics.fillRect(testBoxX, testBoxY, 72, 72);
-    PositionedImage image = new PositionedImage(hero, testBoxX, testBoxY);
-    image.draw(graphics);
-  }
-
-  // To be a KeyListener the class needs to have these 3 methods in it
-  @Override
-  public void keyTyped(KeyEvent e) {
-
-  }
-
-  @Override
-  public void keyPressed(KeyEvent e) {
-
-  }
-
-  // But actually we can use just this one for our goals here
-  @Override
-  public void keyReleased(KeyEvent e) {
-    // When the up or down keys hit, we change the position of our box
-    if (e.getKeyCode() == KeyEvent.VK_UP) {
-      if (testBoxY == 0) {
-        testBoxY = testBoxY;
-      } else {
-        testBoxY -= 72;
-      }
-      hero = "hero-up.png";
-    } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-      if (testBoxY == 648) {
-        testBoxY = testBoxY;
-      } else {
-        testBoxY += 72;
-      }
-      hero = "hero-down.png";
-    } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-      if (testBoxX == 0) {
-        testBoxX = testBoxX;
-      } else {
-        testBoxX -= 72;
-      }
-      hero = "hero-left.png";
-    } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-      if (testBoxX == 648) {
-        testBoxX = testBoxX;
-      } else {
-        testBoxX += 72;
-      }
-      hero = "hero-right.png";
-    }
-    // and redraw to have a new picture with the new coordinates
-    repaint();
-  }
-
+public class main {
   public static void main(String[] args) {
     // Here is how you set up a new window and adding our board to it
     JFrame frame = new JFrame("RPG Game");
@@ -112,6 +16,5 @@ class Board extends JComponent implements KeyListener {
     // Notice (at the top) that we can only do this
     // because this Board class (the type of the board object) is also a KeyListener
   }
-
-
 }
+
