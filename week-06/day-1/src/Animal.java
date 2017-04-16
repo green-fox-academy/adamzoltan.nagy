547eb0f9c68cc8ccd9f3c6c206134ca3b75df32b
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-16 20:37:43 +0200

    Creates Animal abstract class

---
+++ week-06/day-1/src/Animal.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Adam on 2017. 04. 16..
+ */
+public abstract class Animal {
+  private String name;
+  private int age;
+  private String gender;
+  private int weight;
+
+  public abstract void greet();
+  public abstract void wantChild();
+  public abstract void eat();
+}
