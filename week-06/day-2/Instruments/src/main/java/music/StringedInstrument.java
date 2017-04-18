d2653ed35dfa152d73d3be52e64f460d583da56d
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-18 17:18:20 +0200

    Creates abstract class StringedInstrument

---
+++ week-06/day-2/Instruments/src/main/java/music/StringedInstrument.java
@@ -0,0 +1,9 @@
+package main.java.music;
+
+/**
+ * Created by Adam on 2017. 04. 18..
+ */
+public abstract class StringedInstrument extends Instruments {
+  protected int numberOfStrings;
+  final String formatForPlay ="%s, a %d-stringed instrument that %s\n";
+}
