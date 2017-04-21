9ae033bc4eeb95696c507931c5a344c7755ca997
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-21 12:04:29 +0200

    Creates fields, constructor and getter-setter for Card class

--- week-06/day-3/TwentyPlusOne/src/Card.java
+++ week-06/day-3/TwentyPlusOne/src/Card.java
@@ -2,4 +2,37 @@
  * Created by Adam on 2017. 04. 19..
  */
 public class Card {
+  CardColor color;
+  CardSuit suit;
+  CardRank rank;
+
+  public Card(CardColor color, CardSuit suit, CardRank rank) {
+    this.color = color;
+    this.suit = suit;
+    this.rank = rank;
+  }
+
+  public CardColor getColor() {
+    return color;
+  }
+
+  public void setColor(CardColor color) {
+    this.color = color;
+  }
+
+  public CardSuit getSuit() {
+    return suit;
+  }
+
+  public void setSuit(CardSuit suit) {
+    this.suit = suit;
+  }
+
+  public CardRank getRank() {
+    return rank;
+  }
+
+  public void setRank(CardRank rank) {
+    this.rank = rank;
+  }
 }
