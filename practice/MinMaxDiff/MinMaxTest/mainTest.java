ea655c022115d3ff3691b4b7599ea44fd197a8fc
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-27 13:42:44 +0200

    Creates minMaxDiff() 2nd & 3rd test method

--- practice/MinMaxDiff/MinMaxTest/mainTest.java
+++ practice/MinMaxDiff/MinMaxTest/mainTest.java
@@ -9,12 +9,25 @@ import static org.junit.Assert.*;
  */
 public class mainTest {
   @Test
-  public void minMaxDiff() throws Exception {
+  public void minMaxDiffTest1() throws Exception {
     ArrayList<Integer> testList = new ArrayList<>();
     for (int i = 0; i <= 10; i++) {
       testList.add(i);
     }
     assertEquals(10,main.minMaxDiff(testList));
+  }
+
+  @Test
+  public void minMaxDiffTest2() throws Exception {
+    ArrayList<Integer> testList = new ArrayList<>();
+    assertEquals(0,main.minMaxDiff(testList));
+  }
+
+  @Test
+  public void minMaxDiffTest3() throws Exception {
+    ArrayList<Integer> testList = new ArrayList<>();
+    testList.add(1);
+    assertEquals(0,main.minMaxDiff(testList));
   }
 
 }
