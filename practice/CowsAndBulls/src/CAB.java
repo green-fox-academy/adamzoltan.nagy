32450badc0072825ca05930d63df031e40cdb2e1
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-27 11:22:32 +0200

    CAB game works, only with 4 digit number yet

--- practice/CowsAndBulls/src/CAB.java
+++ practice/CowsAndBulls/src/CAB.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,6 +19,52 @@ public class CAB {
   public CAB() {
     this.randomNumber = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
+    System.out.println();
     int userInput = scanner.nextInt();
+    for (int i = 0; i < 4; i++) {
+      int temp = userInput % 10;
+      randomNumber.add(temp);
+      userInput = userInput / 10;
+    }
+    Collections.reverse(randomNumber);
+  }
+
+  public static void gameCAB(int n) {
+    System.out.println("I have a " + n + " digit number. Try to find it out!");
+    boolean victory = false;
+    CAB number = new CAB(n);
+    while (!victory) {
+      int counter = 0;
+      CAB player = new CAB();
+      ArrayList<String> result = new ArrayList<>();
+      for (int i = 0; i < n; i++) {
+        result.add("");
+      }
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (number.getNumber(i) == player.getNumber(j)) {
+            result.set(i, "bull ");
+          }
+        }
+      }
+      for (int i = 0; i < n; i++) {
+        if (number.getNumber(i) == player.getNumber(i)) {
+          result.set(i, "cow ");
+          counter ++;
+        }
+      }
+      for (String cab : result) {
+        System.out.print(cab);
+      }
+      if (counter == n) {
+        System.out.println("\nCongratulations! You found the number!");
+        victory = true;
+      }
+    }
+  }
+
+
+  public int getNumber(int n) {
+    return randomNumber.get(n);
   }
 }
---
+++ practice/CowsAndBulls/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Adam on 2017. 04. 26..
+ */
+public class Main {
+  public static void main(String[] args) {
+
+    CAB.gameCAB(4);
+  }
+}
