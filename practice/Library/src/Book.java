56626feeddc73e405109fe6869157971a4c9f88d
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-27 14:04:35 +0200

    Creates Book class

---
+++ practice/Library/src/Book.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Adam on 2017. 04. 27..
+ */
+public class Book {
+  private String author;
+  private String title;
+  private int releaseYear;
+
+  public Book(String author, String title, int releaseYear) {
+    this.author = author;
+    this.title = title;
+    this.releaseYear = releaseYear;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public int getReleaseYear() {
+    return releaseYear;
+  }
+
+  public void setReleaseYear(int releaseYear) {
+    this.releaseYear = releaseYear;
+  }
+}
