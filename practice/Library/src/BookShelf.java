8199a83c2ccc832eed49d738f8af781ef59fc366
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-04-27 14:12:37 +0200

    Creates BookShelf class

---
+++ practice/Library/src/BookShelf.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Adam on 2017. 04. 27..
+ */
+public class BookShelf {
+  private List<Book> bookshelf;
+
+  public BookShelf() {
+    List<Book> bookshelf = new ArrayList<>();
+  }
+
+  
+}
