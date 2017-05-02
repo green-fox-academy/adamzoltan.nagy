efbf582787fb12df691003884b239259e174549d
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-05-02 16:50:52 +0200

    Creates Hello World Web APP correctly

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
@@ -2,12 +2,19 @@ package com.greenfoxacademy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class HelloWebController {
 
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam("name") String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("count",  HelloRESTController.atomic.incrementAndGet());
     return "greeting";
   }
 }
