063456cb8098c3fda985452e3a3b0eb54de62e1f
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-05-03 14:16:31 +0200

    Exercise 1 now works with Lombok

--- week-07/day-2/lionking/src/main/java/com/greenfoxacademy/controllers/Exercise1.java
+++ week-07/day-2/lionking/src/main/java/com/greenfoxacademy/controllers/Exercise1.java
@@ -4,8 +4,6 @@ import com.greenfoxacademy.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Adam on 2017. 05. 03..
@@ -16,10 +14,8 @@ public class Exercise1 {
 
   @RequestMapping("/")
   public String  bankAccount(Model model) {
-    BankAccount account1 = new BankAccount("Symba", 2000, "lion");
-    model.addAttribute("name",account1.getName());
-    model.addAttribute("balance",account1.getBalance());
-    model.addAttribute("type",account1.getAnimalType());
+    BankAccount Symba = new BankAccount("Symba", 2000, "lion");
+    model.addAttribute("account",Symba);
     return "index";
   }
 }
