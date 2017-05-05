69bccecbf7ad478785e2fba73c1cfd2c2956cd97
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-05-05 10:45:02 +0200

    Creates @Requestmapping(/)

--- week-07/day-4/FoxClub/src/main/java/com/greenfox/MainController.java
+++ week-07/day-4/FoxClub/src/main/java/com/greenfox/MainController.java
@@ -1,7 +1,17 @@
 package com.greenfox;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Adam on 2017. 05. 05..
  */
+
+@Controller
 public class MainController {
+
+  @RequestMapping("/")
+    public String index() {
+    return "index";
+  }
 }
