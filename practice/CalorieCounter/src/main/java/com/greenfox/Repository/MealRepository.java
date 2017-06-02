8d1da5c09d358581985ba4ca0215ccba26d3b892
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-06-02 11:20:03 +0200

    Creates Add new meal button

--- practice/CalorieCounter/src/main/java/com/greenfox/Repository/MealRepository.java
+++ practice/CalorieCounter/src/main/java/com/greenfox/Repository/MealRepository.java
@@ -2,9 +2,11 @@ package com.greenfox.Repository;
 
 import com.greenfox.Model.Meal;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Adam on 2017. 06. 02..
  */
+@Repository
 public interface MealRepository extends CrudRepository<Meal,Long> {
 }
