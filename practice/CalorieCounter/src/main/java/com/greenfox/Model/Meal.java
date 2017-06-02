008afd26e301138dd86ece2796d3fd66cf9fde45
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-06-02 09:53:52 +0200

    Creates Meal class

---
+++ practice/CalorieCounter/src/main/java/com/greenfox/Model/Meal.java
@@ -0,0 +1,31 @@
+package com.greenfox.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by Adam on 2017. 06. 02..
+ */
+@Entity
+@Getter
+@Setter
+public class Meal {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO )
+  private long id;
+  private Date date;
+  private String type;
+  private String description;
+  private double calories;
+
+  public Meal() {
+  }
+
+}
