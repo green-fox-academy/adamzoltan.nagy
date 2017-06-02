7d26abaaf28d624e67bfc5c4d0f4093393e145de
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-06-02 14:47:47 +0200

    Can add new meal to the table

--- practice/CalorieCounter/src/main/java/com/greenfox/Controller/MainController.java
+++ practice/CalorieCounter/src/main/java/com/greenfox/Controller/MainController.java
@@ -1,10 +1,13 @@
 package com.greenfox.Controller;
 
+import com.greenfox.Model.Meal;
 import com.greenfox.Repository.MealRepository;
+import com.greenfox.Repository.MealTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Adam on 2017. 06. 02..
@@ -15,9 +18,26 @@ public class MainController {
   @Autowired
   private MealRepository mealRepository;
 
+  @Autowired
+  private MealTypeRepository mealTypeRepository;
+
   @RequestMapping("/index")
   public String index(Model model) {
     model.addAttribute("meal", mealRepository.findAll());
     return "index";
+  }
+
+  @RequestMapping("/addMeal")
+  public String addMeal(Model model) {
+    model.addAttribute("mealTypes", mealTypeRepository.findAll());
+    return "addMeal";
+  }
+
+  @RequestMapping("/saveMeal")
+  public String saveMeal(@RequestParam (name="type") String type,
+                         @RequestParam (name="description") String description,
+                         @RequestParam (name="calories") Double calories) {
+    mealRepository.save(new Meal(type,description,calories));
+    return "redirect:/index";
   }
 }
