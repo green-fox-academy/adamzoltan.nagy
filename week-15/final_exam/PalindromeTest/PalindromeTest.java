94a178421aea167f653a4c4a8e9109f01de46cb5
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-07-10 09:55:50 +0200

    Add unit test for isPalindrome

---
+++ week-15/final_exam/PalindromeTest/PalindromeTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Adam on 2017. 07. 10..
+ */
+class PalindromeTest {
+  String palindrome = "görög";
+  String notPalindrome = "alma";
+
+  @Test
+  public void testPalindromeWithPalindrome() throws Exception {
+    assertEquals(true, Palindrome.isPalindrome(palindrome));
+  }
+
+  @Test
+  public void testPalindromeWithNonPalindrome() throws Exception {
+    assertEquals(false, Palindrome.isPalindrome(notPalindrome));
+  }
+
+}
