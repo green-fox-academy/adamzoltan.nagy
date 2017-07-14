d26451dff55639f200424efe9cde0381e8aef45b
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-07-14 14:41:32 +0200

    Get even numbers from an array with LINQ exp

---
+++ week-15/c#/LINQ/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Write a LINQ Expression to get the even numbers from the following array:
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            int [] evenNumbers = n.Where( i => i % 2 == 0).ToArray();
+            foreach (var item in evenNumbers)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
